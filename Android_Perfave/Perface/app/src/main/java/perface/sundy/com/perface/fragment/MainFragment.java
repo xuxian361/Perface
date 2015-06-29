726a3fa26e7d312d61dbd2cefc046351a92c86f5
Sundy_Xu <sundy_xu@mtelnet.com>
2015-06-29 11:13:26 +0800

    update main

--- Android_Perfave/Perface/app/src/main/java/perface/sundy/com/perface/fragment/MainFragment.java
+++ Android_Perfave/Perface/app/src/main/java/perface/sundy/com/perface/fragment/MainFragment.java
@@ -1,18 +1,22 @@
 package perface.sundy.com.perface.fragment;
 
 import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.TextView;
 
 import com.androidquery.AQuery;
 
 import perface.sundy.com.perface.R;
 import perface.sundy.com.perface.adapter.BookmarkGridAdapter;
+import perface.sundy.com.perface.utils.custom_view.FixedGridLayout;
 
 /**
  * Created by sundy on 15/6/23.
@@ -50,6 +54,7 @@ public class MainFragment extends BaseFragment {
 
     private void init() {
         aq.id(R.id.txt_title).text(R.string.home).visible();
+        aq.id(R.id.txt_more).clicked(onClickListener);
         adapter = new BookmarkGridAdapter(context, inflater);
         gv_photos = aq.id(R.id.gv_photos).getGridView();
         gv_photos.setAdapter(adapter);
@@ -107,7 +112,67 @@ public class MainFragment extends BaseFragment {
         @Override
         public void onClick(View view) {
             switch (view.getId()) {
+                case R.id.txt_more:
+                    rtLog(TAG, "----------->txt_more");
+                    View dialog_view = inflater.inflate(R.layout.dialog_more_option, null);
+                    final Dialog dialog = new Dialog(context, R.style.dialog);
+                    dialog.setContentView(dialog_view);
+                    AQuery aq1 = new AQuery(dialog_view);
+                    FixedGridLayout fixedv_style = (FixedGridLayout) aq1.id(R.id.fixedv_style).getView();
+                    if (fixedv_style.getChildCount() > 0) {
+                        fixedv_style.removeAllViews();
+                    }
+                    DisplayMetrics dm = new DisplayMetrics();
+                    context.getWindowManager().getDefaultDisplay().getMetrics(dm);
+                    if (dm.heightPixels == 480 && dm.widthPixels == 800) {
+                        fixedv_style.setCountpuls(4);
+                    } else {
+                        fixedv_style.setCountpuls(5);
+                    }
 
+                    if (dm.heightPixels == 1080 && dm.widthPixels == 1920) {
+                        fixedv_style.setCellHeight(90);
+                        fixedv_style.setmCelllong(350);
+                    } else if (dm.heightPixels == 720 && dm.widthPixels == 1196 || dm.widthPixels == 1184) {
+                        fixedv_style.setmCelllong(220);
+                    }
+                    for (int i = 0; i < 103; i++) {
+                        TextView text = new TextView(context);
+                        text.setText("style " + i);
+                        text.setPadding(10, 10, 10, 10);
+                        text.setBackgroundResource(R.color.white_bg);
+                        text.setClickable(true);
+                        text.setTextColor(Color.rgb(0, 0, 0));
+                        fixedv_style.addView(text);
+
+                        final int finalI = i;
+                        text.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                //To change body of implemented methods use File | Settings | File Template.
+                                rtLog(TAG, "----------->URI finalI= " + finalI);
+
+                            }
+                        });
+                    }
+                    aq1.id(R.id.btn_confirm).clicked(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            rtLog(TAG, "----------->URI btn_update_name= ");
+                            dialog.cancel();
+                            dialog.dismiss();
+
+                        }
+                    });
+                    aq1.id(R.id.btn_cancel).clicked(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.cancel();
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.show();
+                    break;
             }
         }
     };
---
+++ Android_Perfave/Perface/app/src/main/java/perface/sundy/com/perface/utils/custom_view/FixedGridLayout.java
@@ -0,0 +1,156 @@
+package perface.sundy.com.perface.utils.custom_view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sundy on 15/6/29.
+ */
+public class FixedGridLayout extends ViewGroup {
+
+    private int mCellWidth;
+    private int mCellHeight;
+    private int mCountpuls;
+    private final static int VIEW_MARGIN = 10;
+    private int mCelllong;
+
+    public FixedGridLayout(Context context) {
+        super(context);
+    }
+
+    public FixedGridLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        //初始化单元格宽和高
+        mCellWidth = 60;
+        mCellHeight = 70;
+        mCelllong = 200;
+        mCountpuls = 1;
+    }
+
+    public void setCountpuls(int w) {
+        mCountpuls = w;
+    }
+
+    public void setmCelllong(int listlong) {
+        mCelllong = listlong;
+    }
+
+    //设置单元格宽度
+    public void setCellWidth(int w) {
+        mCellWidth = w;
+        requestLayout();
+    }
+
+    //设置单元格高度
+    public void setCellHeight(int h) {
+        mCellHeight = h;
+        requestLayout();
+    }
+
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        //获取布局控件宽高
+        int width = getWidth();
+        int height = getHeight();
+        //创建画笔
+        Paint mPaint = new Paint();
+        //设置画笔的各个属性
+        mPaint.setColor(Color.WHITE);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(0);
+        mPaint.setAntiAlias(true);
+
+        //创建矩形框
+        Rect mRect = new Rect(0, 0, width, height);
+        //绘制边框
+        canvas.drawRect(mRect, mPaint);
+
+        //最后必须调用父类的方法
+        super.dispatchDraw(canvas);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //创建测量参数
+        int cellWidthSpec = MeasureSpec.makeMeasureSpec(mCellWidth, MeasureSpec.AT_MOST);
+        int cellHeightSpec = MeasureSpec.makeMeasureSpec(mCellHeight, MeasureSpec.AT_MOST);
+
+        int measureWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int measureHeigth = MeasureSpec.getSize(heightMeasureSpec);
+
+
+        //记录ViewGroup中Child的总个数
+        int count = getChildCount();
+
+        //设置子空间Child的宽高
+        for (int i = 0; i < count; i++) {
+            View childView = getChildAt(i);
+            int widthSpec = 0;
+            int heightSpec = 0;
+            LayoutParams params = childView.getLayoutParams();
+
+            if (params.width > 0) {
+                widthSpec = MeasureSpec.makeMeasureSpec(params.width, MeasureSpec.EXACTLY);
+            } else if (params.width == -1) {
+                widthSpec = MeasureSpec.makeMeasureSpec(measureWidth, MeasureSpec.EXACTLY);
+            } else if (params.width == -2) {
+                widthSpec = MeasureSpec.makeMeasureSpec(measureWidth, MeasureSpec.AT_MOST);
+            }
+
+            if (params.height > 0) {
+                heightSpec = MeasureSpec.makeMeasureSpec(params.height, MeasureSpec.EXACTLY);
+            } else if (params.height == -1) {
+                heightSpec = MeasureSpec.makeMeasureSpec(measureHeigth, MeasureSpec.EXACTLY);
+            } else if (params.height == -2) {
+                heightSpec = MeasureSpec.makeMeasureSpec(measureHeigth, MeasureSpec.AT_MOST);
+            }
+            childView.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED);
+        }
+
+        //设置容器控件所占区域大小
+        //注意setMeasuredDimension和resolveSize的用法
+        setMeasuredDimension(resolveSize(mCellWidth, widthMeasureSpec), resolveSize(mCellHeight * mCountpuls, heightMeasureSpec));
+        //setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
+
+        //不需要调用父类的方法
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        final int count = getChildCount();
+        int row = 0;// which row lay you view relative to parent
+        int lengthX = l;    // right position of child relative to parent
+        int lengthY = t;    // bottom position of child relative to parent
+        for (int i = 0; i < count; i++) {
+
+            final View child = this.getChildAt(i);
+            int width = child.getMeasuredWidth();
+            int height = child.getMeasuredHeight();
+            if (i == 0) {
+                lengthX = width;
+            } else {
+                lengthX += width + VIEW_MARGIN;
+            }
+
+            lengthY = row * (height + VIEW_MARGIN) + VIEW_MARGIN + height + t;
+
+            //if it can't drawing on a same line , skip to next line
+            if (lengthX > r - mCelllong) {
+                lengthX = width;
+                row++;
+                lengthY = row * (height + VIEW_MARGIN) + VIEW_MARGIN + height + t;
+            }
+
+            child.layout(lengthX - width, lengthY - height, lengthX, lengthY);
+        }
+    }
+}
