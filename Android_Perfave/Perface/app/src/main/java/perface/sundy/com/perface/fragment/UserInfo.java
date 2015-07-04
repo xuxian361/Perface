fa8caf2fda7fd5ddc7248b77723323bc3a414538
Sundy_Xu <sundy_xu@mtelnet.com>
2015-07-04 18:42:20 +0800

    update userinfo

--- Android_Perfave/Perface/app/src/main/java/perface/sundy/com/perface/fragment/UserInfo.java
+++ Android_Perfave/Perface/app/src/main/java/perface/sundy/com/perface/fragment/UserInfo.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.androidquery.AQuery;
 
@@ -13,11 +14,12 @@ import perface.sundy.com.perface.R;
 /**
  * Created by sundy on 15/6/24.
  */
-public class UserInfo extends BaseFragment {
+public class UserInfo extends BaseFragment implements View.OnClickListener {
 
     private final String TAG = "UserInfo";
     private View v;
     private Fragment fragment;
+    private LinearLayout linear_password;
 
     public UserInfo() {
     }
@@ -42,6 +44,33 @@ public class UserInfo extends BaseFragment {
     }
 
     private void init() {
+        aq.id(R.id.txt_title).text(R.string.my_info).visible();
+        linear_password = (LinearLayout) aq.id(R.id.linear_password).getView();
+        aq.id(R.id.img_arrow).clicked(this);
+        aq.id(R.id.btn_confirm).clicked(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        super.onClick(view);
+        switch (view.getId()) {
+            case R.id.img_arrow:
+                if (linear_password.getVisibility() == View.VISIBLE) {
+                    linear_password.setVisibility(View.GONE);
+                    aq.id(R.id.btn_confirm).gone();
+                    aq.id(R.id.img_arrow).image(R.mipmap.arrow_down_black2);
+                } else {
+                    linear_password.setVisibility(View.VISIBLE);
+                    aq.id(R.id.btn_confirm).visible();
+                    aq.id(R.id.img_arrow).image(R.mipmap.arrow_up_black2);
+                }
+                break;
+            case R.id.btn_confirm:
+                linear_password.setVisibility(View.GONE);
+                aq.id(R.id.btn_confirm).gone();
+                aq.id(R.id.img_arrow).image(R.mipmap.arrow_down_black2);
+                break;
+        }
     }
 
     @Override
